73d0eb4cfdb204aa0321c27f47ec435516ca4f90
Igor <igor.flores96@hotmail.com>
2025-02-11 16:55:59 -0300

    add: adicionado sistema de emails. Agora usuario pode abrir os emails para visualizar as mensagens

--- UnityProject/Assets/_Scripts/TextDialogue/EmailHandler.cs
+++ UnityProject/Assets/_Scripts/TextDialogue/EmailHandler.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EmailHandler : MonoBehaviour
 {
     [SerializeField] private List<SO_Email> _emailsToSend = new List<SO_Email>();
     [SerializeField] private GameObject _emailPrefab;
-    [SerializeField] private Transform _emailParent;
+    [SerializeField] private Transform _homeEmailCanvas;
+    [SerializeField] private Transform _emailCanvas;
+    [SerializeField] private TextMeshProUGUI _emailTitle;
+    [SerializeField] private TextMeshProUGUI _emailSender;
+    [SerializeField] private TextMeshProUGUI _emailContent;
     private List<Email> _currentEmails = new List<Email>();
     private int _currentEmailSended = 0;
+    private Dictionary<GameObject, Email> _emailsInstanciados = new Dictionary<GameObject, Email>();
 
     private void OnEnable() 
     {
@@ -19,12 +25,40 @@ public class EmailHandler : MonoBehaviour
     {
         Email email = new Email(_emailsToSend[_currentEmailSended]);
 
-        GameObject instanceEmail = Instantiate(_emailPrefab, Vector3.down * 50f, Quaternion.identity);
-        instanceEmail.transform.SetParent(_emailParent);
+        GameObject instanceEmail = Instantiate(_emailPrefab, Vector3.zero, Quaternion.identity);
+        instanceEmail.transform.SetParent(_homeEmailCanvas);
+        instanceEmail.name = email.Title;
+        instanceEmail.transform.localScale = new Vector3(1, 1, 1);
+        instanceEmail.transform.localPosition = new Vector3(-960f, 350f + (_currentEmailSended * -50f), 0);
 
         if(instanceEmail.TryGetComponent(out EmailInstance instance))
+        {
             instance.UpdateInfos(sender: email.Sender, title: email.Title, contentSmall: email.Content);
+            instance.OnClickEmail.AddListener(OpenEmail);
+        }
+
+        if(!_emailsInstanciados.ContainsKey(instanceEmail))
+            _emailsInstanciados.Add(instanceEmail, email);
         
         _currentEmailSended++;
+    }
+
+    public void OpenEmail(GameObject email)
+    {
+        _homeEmailCanvas.gameObject.SetActive(false);
+        _emailCanvas.gameObject.SetActive(true);
+
+        if(_emailsInstanciados.TryGetValue(email, out Email instance))
+        {
+            _emailTitle.text = instance.Title;
+            _emailContent.text = instance.Content;
+            _emailSender.text = instance.Sender;
+        }
+    }
+
+    public void CloseEmail()
+    {
+        _homeEmailCanvas.gameObject.SetActive(true);
+        _emailCanvas.gameObject.SetActive(false);
     }
 }
--- UnityProject/Assets/_Scripts/TextDialogue/EmailInstance.cs
+++ UnityProject/Assets/_Scripts/TextDialogue/EmailInstance.cs
@@ -1,16 +1,35 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class EmailInstance : MonoBehaviour
 {
+    public UnityEvent<GameObject> OnClickEmail;
+    [SerializeField] private Button _emailButton;
     [SerializeField] private TextMeshProUGUI _sender;
     [SerializeField] private TextMeshProUGUI _title;
     [SerializeField] private TextMeshProUGUI _contentSmall;
+
+    private void OnEnable()
+    {
+        _emailButton.onClick.AddListener(ClickEmail);
+    }
+
+    private void OnDisable()
+    {
+        _emailButton.onClick.RemoveListener(ClickEmail);
+    }
 
     public void UpdateInfos(string sender, string title, string contentSmall)
     {
         _sender.text = sender;
         _title.text = title;
         _contentSmall.text = contentSmall;
+    }
+
+    private void ClickEmail()
+    {
+        OnClickEmail?.Invoke(this.gameObject);
     }
 }
