3800ce84c0f747bdb736af957906c16f6ac880ca
Igor <igor.flores96@hotmail.com>
2025-07-11 14:59:57 -0300

    fix: fixed problem with n2 fluxogram.

--- UnityProject/Assets/_Scripts/Interfaces Concrect States/DayOne/Day_One_EmailSecondChoiceResponse_Rafael.cs
+++ UnityProject/Assets/_Scripts/Interfaces Concrect States/DayOne/Day_One_EmailSecondChoiceResponse_Rafael.cs
@@ -2,7 +2,7 @@ public class Day_One_EmailSecondChoiceResponse_Rafael : IEmailStateHandler
 {
     public void Handle(IEmailContext context)
     {
-        EventManager.SpawnEmail(EmailType.SPAM);
+        EventManager.SpawnEmail(EmailType.LORE);
         EventManager.SpawnEmail(EmailType.NEWS);
         EventManager.TicketObjectiveCompleted();
     }
