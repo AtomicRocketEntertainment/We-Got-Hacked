260f9ee48ceadf92fdb96242bbaf5ad05644e809
Igor <igor.flores96@hotmail.com>
2025-09-19 11:35:30 -0300

    add: added two more emails for Rafael day 2.

--- UnityProject/Assets/_Scripts/Interfaces Concrect States/DayTwo/Choices/Day_Two_SecondMalwhereChoice_Rafael.cs
+++ UnityProject/Assets/_Scripts/Interfaces Concrect States/DayTwo/Choices/Day_Two_SecondMalwhereChoice_Rafael.cs
@@ -3,5 +3,6 @@ public class Day_Two_SecondMalwhereChoice_Rafael : IChoiceStateHandler
     public void Handle(IChoiceContext context)
     {
         EventManager.EnablePlayerWriteEmail();
+        EventManager.SpawnEmail(EmailType.NEWS);
     }
 }
--- UnityProject/Assets/_Scripts/Interfaces Concrect States/DayTwo/Writes/Day_Two_FirstWrite_Rafael.cs
+++ UnityProject/Assets/_Scripts/Interfaces Concrect States/DayTwo/Writes/Day_Two_FirstWrite_Rafael.cs
@@ -3,6 +3,7 @@ public class Day_Two_FirstWrite_Rafael : IEmailStateHandler
     public void Handle(IEmailContext context)
     {
         EventManager.SpawnEmail(EmailType.NEWS);
+        EventManager.SpawnEmail(EmailType.SPAM);
         EventManager.DisablePlayerWriteEmail();
         EventManager.TicketObjectiveCompleted();
         context.ChangeWriteState(HistoryPartState.Part_Two);
