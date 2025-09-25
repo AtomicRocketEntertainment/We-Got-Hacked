059e9552b4f9818eccd67cd2d44a7a8e123aaa37
Igor <igor.flores96@hotmail.com>
2025-09-25 15:56:27 -0300

    add: advanced game N1's story until lore 5.

--- UnityProject/Assets/_Scripts/Emails/EmailHandler.cs
+++ UnityProject/Assets/_Scripts/Emails/EmailHandler.cs
@@ -47,6 +47,9 @@ public class EmailHandler : MonoBehaviour, INeedOpenCanvas
     private readonly string _lore4DayTwo = "Lore 4 Day 2";
     private readonly string _lore8DayTwo = "Lore 8 Day 2";
     private readonly string _lore11DayTwo = "Lore 11 Day 2";
+    private readonly string _news2Day3 = "News 2 Day 3";
+    private readonly string _lore4DayThree = "Lore 4 Day 3";
+
     private readonly string _loreA04 = "Lore A04";
     
     private const int LORE_TO_OPEN_WRITE_EMAIL = 5;
@@ -96,7 +99,13 @@ public class EmailHandler : MonoBehaviour, INeedOpenCanvas
         if(emailIndex == _lore8DayTwo)
             return;
 
-        if (emailIndex == _lore9DayOne || emailIndex == _lore12DayOne || emailIndex == _lore4DayTwo || emailIndex == _loreA04 || _lore11DayTwo == emailIndex)
+        if (emailIndex == _lore9DayOne
+        || emailIndex == _lore12DayOne
+        || emailIndex == _lore4DayTwo
+        || emailIndex == _loreA04
+        || _lore11DayTwo == emailIndex
+        || _news2Day3 == emailIndex
+        || _lore4DayThree == emailIndex)
                 _writeEmailState = WriteEmailState.CanWrite;
         else
             _writeEmailState = WriteEmailState.CantWrite;
