c55c4ec1ca503860bc9271e19d8c9d56cd55f77e
Igor <igor.flores96@hotmail.com>
2025-09-30 10:16:14 -0300

    add: added a new lore email on day 3 N1 fluxogram.

--- UnityProject/Assets/_Scripts/Interfaces Concrect States/DayThree/Writes/Day_Three_SecondWrite_Tiago.cs
+++ UnityProject/Assets/_Scripts/Interfaces Concrect States/DayThree/Writes/Day_Three_SecondWrite_Tiago.cs
@@ -3,6 +3,7 @@ public class Day_Three_SecondWrite_Tiago : IEmailStateHandler
     public void Handle(IEmailContext context)
     {
         EventManager.SpawnEmail(EmailType.SPAM);
+        EventManager.SpawnEmail(EmailType.LORE);
         context.ChangeWriteState(HistoryPartState.Part_Three);
     }
 }
