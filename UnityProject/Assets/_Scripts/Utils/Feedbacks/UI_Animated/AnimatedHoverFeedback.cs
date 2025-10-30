16ebebb2ea59d1bb2a53ea54a5af545c31728b32
Igor <igor.flores96@hotmail.com>
2025-10-30 15:43:43 -0300

    add: added animation script to UI elements. add: animated auction buttons.

---
+++ UnityProject/Assets/_Scripts/Utils/Feedbacks/UI_Animated/AnimatedHoverFeedback.cs
@@ -0,0 +1,14 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimatedHoverFeedback : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [BoxGroup("Animated Seetings"), SerializeField] private float _animTime = 0.2f;
+    [BoxGroup("Animated Seetings"), SerializeField] private LeanTweenType _tweenCurve = LeanTweenType.easeOutCubic;
+    [BoxGroup("Animated Seetings"), SerializeField] private Vector3 _scaleToGo = new Vector3(1.1f, 1.1f, 1.1f);
+
+
+    public void OnPointerEnter(PointerEventData eventData) => LeanTween.scale(this.gameObject, _scaleToGo, _animTime).setEase(_tweenCurve);
+    public void OnPointerExit(PointerEventData eventData) => LeanTween.scale(this.gameObject, Vector3.one, _animTime).setEase(_tweenCurve);
+}
