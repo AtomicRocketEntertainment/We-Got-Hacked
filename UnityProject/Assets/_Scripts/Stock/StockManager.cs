a0e78e1a8227c0e7b4e5dca84ef3af3b3c368c13
NicolayHGit <nicolayhoff@gmail.com>
2026-06-29 15:48:22 -0300

    changes

--- UnityProject/Assets/_Scripts/Stock/StockManager.cs
+++ UnityProject/Assets/_Scripts/Stock/StockManager.cs
@@ -67,7 +67,6 @@ public class StockManager : MonoBehaviour, INeedOpenCanvas
 
     private void OnEnable()
     {
-        Debug.Log("StockManager OnEnable: Subscribing to events.");
         EventManager.OnCorrectChoice += CorrectChoice;
         EventManager.OnWrongChoice += WrongChoice;
 
@@ -188,8 +187,6 @@ public class StockManager : MonoBehaviour, INeedOpenCanvas
         CalculateGraphBounds(out _yMininumValue, out _yMaximumValue);
         CreateSeparators();
         UpdateStockGraph();
-        int valueDay = _stockCalculation.GetValueDay();
-        PopupNotification.Instance?.OnStockUp(_playerCompany, valueDay);
         EventManager.NotifyBrowser();
     }
 
@@ -200,10 +197,6 @@ public class StockManager : MonoBehaviour, INeedOpenCanvas
         CalculateGraphBounds(out _yMininumValue, out _yMaximumValue);
         CreateSeparators();
         UpdateStockGraph();
-        Debug.Log("Wrong choice, Stock is down! Showing popup notification.");
-        int minValue = _stockCalculation.GetMinValue();
-        int valueDay = _stockCalculation.GetValueDay();
-        PopupNotification.Instance?.OnStockDown(_playerCompany, valueDay, minValue);
         EventManager.NotifyBrowser();
     }
 
