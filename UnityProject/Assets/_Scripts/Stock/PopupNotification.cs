d4e83235f8af6e310ba968b9ac930b72e8ea69d0
NicolayHGit <nicolayhoff@gmail.com>
2026-07-06 14:48:34 -0300

    Update PopupNotification.cs

--- UnityProject/Assets/_Scripts/Stock/PopupNotification.cs
+++ UnityProject/Assets/_Scripts/Stock/PopupNotification.cs
@@ -99,7 +99,7 @@ public class PopupNotification : MonoBehaviour
             int lastPetroCaisStock = values[values.Count - 1];
 
             int remainingErrors = Mathf.Max(0, ((lastPetroCaisStock - minValue) / valueDay) - 1);
-            string remainingMsg = (remainingErrors > 0) ? $"Agora você só pode cometer mais {remainingErrors} erros" : $"Cuidado essa é sua ultima chance";
+            string remainingMsg = (remainingErrors > 1) ? $"Agora você só pode cometer mais {remainingErrors} erros" : $"Cuidado essa é sua ultima chance";
             string message = $"Decisão incorreta! Perda de {valueDay} pontos, {remainingMsg}";
             ShowPopup(message);
         }
